081c806093caf0f5d9b1f53a668db878be9ef8ae
Nerea Trillo <a21nereatp@iessanclemente.net>
2022-06-11 23:09:05 +0200

    documentouse a clase Conta e xerouse o Javadoc do proxectoBanco

--- src/main/java/proxecto/proxectobanco/Conta.java
+++ src/main/java/proxecto/proxectobanco/Conta.java
@@ -2,18 +2,35 @@ package proxecto.proxectobanco;
 
 /**
  *
- * @author profesor
+ * @author Nerea Trillo
  */
 public class Conta {
+
+    /**
+     * Define a variable titular
+     */
     protected Persoa titular;
+
+    /**
+     * Define as variables numero y saldo
+     */
     protected String numero;
     private float saldo;
 
+    /**
+     * Define o construtor Conta
+     */
     public Conta() {
         titular=null;
         numero=null;
         saldo=1000f;
     }
+
+    /**
+     *
+     * @param valor
+     * @return expresión booleana: verdadeiro ou falso
+     */
     public boolean retirar(float valor){
         if (getSaldo()-valor<0)
             return false;
@@ -21,10 +38,18 @@ public class Conta {
         return true;
     }
     
+    /**
+     *
+     * @param valor para o método ingresar
+     */
     public void ingresar(float valor){
         setSaldo(getSaldo() + valor);
     }
     
+    /**
+     *
+     * @param valor para o método inicializarSaldo
+     */
     public void inicializarSaldo(float valor){ 
         this.saldo=0.00f; 
     }
